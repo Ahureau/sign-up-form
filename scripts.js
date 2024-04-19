252c9a6cc67aa91b58d5bafb9d07d75e0f844bec
Alex H <53427235+Ahureau@users.noreply.github.com>
2024-04-19 15:20:27 +0200

    Figured out html novalidate and js validity.
    The fields that have html validation can still
    make use of the validity API. I'll be using this
    to change how the error looks in the DOM.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,48 @@
-const button = document.getElementById("formButton");
-const password = document.getElementById("password");
-const passwordConfirm = document.getElementById("passwordConfirm");
-const passwordError = document.querySelector("#password + .errorContainer .errorText");
+// Global variables
 
-function passwordCheck() {
+const passwordConfirm = document.getElementById("passwordConfirm");
+const password = document.getElementById("password");
+const form = document.querySelector(".formFields .theForm");
+
+// Only submit if all is valid
+
+form.addEventListener("submit", (event) => {
+    if (password.parentNode.querySelector(".errorContainer .errorText").classList.contains("visible")) {
+        confirmCheck();
+        event.preventDefault();
+    }
+});
+
+
+// Password & password check functions
+
+function confirmCheck() {
+    const confirmError = document.querySelector("#passwordConfirm + .errorContainer .errorText");
+    const confirmContainer = document.querySelector("#passwordConfirm");
     if (passwordConfirm.value !== password.value) {
-        passwordError.classList.add("visible");
+        confirmError.classList.add("visible");
+        confirmContainer.classList.add("userInvalid");
+        return false;
     } else {
-        passwordError.classList.remove("visible");
+        confirmError.classList.remove("visible");
+        confirmContainer.classList.remove("userInvalid");
+        return true;
     }
 }
 
-passwordConfirm.addEventListener("focusout", passwordCheck);
+function passwordCheck() {
+    const passwordError = document.querySelector("#password + .errorContainer .errorText");
+    const passwordContainer = document.querySelector("#password");
+    if (!password.validity.valid) {
+        passwordError.classList.add("visible");
+        passwordContainer.classList.add("userInvalid");
+        return false;
+    } else {
+        passwordError.classList.remove("visible");
+        passwordContainer.classList.remove("userInvalid");
+        return true;
+    }
+}
+
+passwordConfirm.addEventListener("focusout", confirmCheck);
+password.addEventListener("focusout", () => {confirmCheck(); passwordCheck()});
